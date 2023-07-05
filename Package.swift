dd504547fa08d67bf8ec161c541be21c57882dc5
MapLibreGL <vsang25@gmail.com>
2023-07-05 13:15:56 +0700

    #Change - Update #1

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
         .binaryTarget(
             name: "Mapbox",
             url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v5.12.2/Mapbox-5.12.2.zip",
-            checksum: "348453495bad198b380e033a6bc6e6134bf71be27116904a25152b93a2d77491")
+            checksum: "6f52b877c639c022f59984b346d0758cc7e34ac19d8b2110d0444df32bfe140f")
     ]
 )
