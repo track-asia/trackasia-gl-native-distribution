8fc27e57eafdda35a9583be640a944d82be8dd6f
MapLibreGL <vsang25@gmail.com>
2023-07-05 12:00:50 +0700

    #Change - Update Mapbox

--- Package.swift
+++ Package.swift
@@ -10,16 +10,16 @@ let package = Package(
     ],
     dependencies: [
     ],    
-    targets: [
-        .binaryTarget(
-            name: "Mapbox",
-            url: "https://github.com/maplibre/maplibre-native/releases/download/ios-v5.13.0/Mapbox-5.13.0.zip",
-            checksum: "6f52b877c639c022f59984b346d0758cc7e34ac19d8b2110d0444df32bfe140f")
-    ]
     // targets: [
     //     .binaryTarget(
     //         name: "Mapbox",
-    //         url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v5.12.2/Mapbox-5.12.2.zip",
-    //         checksum: "479ab825d3b2411cdc309cbcc51ea904")
+    //         url: "https://github.com/maplibre/maplibre-native/releases/download/ios-v5.13.0/Mapbox-5.13.0.zip",
+    //         checksum: "348453495bad198b380e033a6bc6e6134bf71be27116904a25152b93a2d77491")
     // ]
+    targets: [
+        .binaryTarget(
+            name: "Mapbox",
+            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v5.12.2/Mapbox-5.12.2.zip",
+            checksum: "348453495bad198b380e033a6bc6e6134bf71be27116904a25152b93a2d77491")
+    ]
 )
