98add175461276618eb6938d0356e9d9a518e2e6
Sang Nguyen <vsang25@gmail.com>
2023-08-09 15:33:41 +0700

    #Change - Update code

--- Package.swift
+++ Package.swift
@@ -5,15 +5,15 @@ let package = Package(
     name: "Trackasia Native",
     products: [
         .library(
-            name: "Trackasia",
-            targets: ["Trackasia"])
+            name: "Mapbox",
+            targets: ["Mapbox"])
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
-            name: "Trackasia",
-            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.0/Trackasia-1.0.0.zip",
+            name: "Mapbox",
+            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.0/Mapbox-1.0.0.zip",
             checksum: "6f52b877c639c022f59984b346d0758cc7e34ac19d8b2110d0444df32bfe140f")
     ]
 )
