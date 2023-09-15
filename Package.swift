9257258c93d2879db5a55fc9e5f780f9f887d0e7
Sang Nguyen <vsang25@gmail.com>
2023-09-15 08:25:03 +0700

    #Change - Update Version 1.0.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.1/Mapbox-1.0.1.zip",
+            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.2/Mapbox-1.0.2.zip",
             //checksum: "85a2842a8d5cd682c5736512b197dde2d391a8559df9d939bc90ab1bd4bbba49")
             checksum: "1d3cda05c283373b5df111bb55b11df88cde93cd34800ca7469d2e597d1328a0")
     ]
