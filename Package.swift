93481b8c93342285c1f1a5748a14980a259fb362
Sang Nguyen <vsang25@gmail.com>
2024-07-16 11:10:51 +0700

    #Change - Upload CheckSum 1.0.5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrackAsia",
-            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.4/TrackAsia-v1.0.4.zip",
+            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v1.0.5/TrackAsia-v1.0.5.zip",
             checksum: "b9be74f08b3b7f0350df1fce9f7436e0508ff970b9b70e266a77ba6e500b9f85")
     ]
 )
