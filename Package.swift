c595d5d56ddd540fc3b41299ac7b44b5e9d9c43f
Sang Nguyen <vsang25@gmail.com>
2025-03-06 09:44:38 +0700

    #Change - Upload Version 2.0.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrackAsia",
-            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v2.0.1/TrackAsia.dynamic.xcframework.zip",
+            url: "https://github.com/track-asia/trackasia-native/releases/download/ios-v2.0.2/TrackAsia.dynamic.xcframework.zip",
             checksum: "04cd699fc6d2213355226d6f0a1b6b5e048f29178b688780d2d779f1b5c93b8b")
             
     ]
